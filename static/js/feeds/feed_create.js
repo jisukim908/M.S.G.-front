929a590bd7356aa8641a2ce12c1b138c4dd91229
dana <bear9473@gmail.com>
2023-05-15 05:59:37 +0900

    feature: feed create 기능. *cors-header오류로 코드테스트 불가

--- static/js/feeds/feed_create.js
+++ static/js/feeds/feed_create.js
@@ -1,43 +1,103 @@
+const backend_base_url = "http://127.0.0.1:8000"
+const frontend_base_url = "http://127.0.0.1:5501"
+
 console.log("hey")
 
+window.onload = async function loadTag() {
 
+  const response_tag = await fetch(`${backend_base_url}/users/tag/`, {
+        method : 'GET'
+    });
 
+    response_json_tag = await response_tag.json()
+    const tags = document.getElementById("tags")
+    response_json_tag.forEach(tag => {
+        const newInput = document.createElement('input')
+        newInput.setAttribute("type", "checkbox")
+        newInput.setAttribute("name", "tag")
+        newInput.setAttribute("value", tag['id'])
+        newInput.setAttribute("id", tag['name'])
+        newInput.setAttribute("class", 'check')
+        const newTag = document.createElement('label')
+        newTag.setAttribute("class", "tag-input")
+        newTag.innerText = tag['name']
+        tags.appendChild(newTag).appendChild(newInput)
 
-function displayLocalFile (strFileName) {
-    var url = 'file:/media/' + strFileName;
-    if (document.layers && location.protocol.toLowerCase() != 'file:' && navigator.javaEnabled())
-      netscape.security.PrivilegeManager.enablePrivilege('UniversalFileRead');
-    open (url, 'preview');
+    })
+
   }
+
+async function handlePost(){
+  user = localStorage.getItem("payload")
+  user_id = user.slice(-2)[0]
+  
+  const title = document.getElementById("title").value
+  const context = document.getElementById("context").value
+  const image = document.getElementById("image").src
+  console.log(image)
+
+  check_video_key = document.getElementById("result")
+  console.log(check_video_key)
+  if(check_video_key === null){
+  } else {
+    const video_key = document.getElementById("video_key").value
+  }
+  const query = 'input[name="tag"]:checked';
+  const selectedEls = document.querySelectorAll(query)
+  const tag = []
+  selectedEls.forEach((el) => {
+      tag.push(parseInt(el.value))
+  })
+  console.log(title)
+
+  if(check_video_key === null){
+    stringify = JSON.stringify({
+      "title":title,
+      "context":context,
+      'image':image,
+      'tag':tag
+    })
+  } else {  
+    stringify = JSON.stringify({
+      "title":title,
+      "context":context,
+      'image':image,
+      'video_key':video_key, 
+      'tag':tag
+    })
+  }
+
+  const request_post = await fetch(`${backend_base_url}/create_feed/`, {
+    headers:{
+      headers: {
+        "access-control-allow-origin": "*",
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': '*',
+      },
+      'Authorization': 'Bearer ' + localStorage.getItem("access"),
+      'contect-type' : 'application/json',
+  },
+  method:"POST",
+  body: stringify
+  });
+console.log("포스트 성공")
+// location.href = 'channelinfo.html'
+}
 
 function preview(input) {
   if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function(e) {
-          document.getElementById("profile_image").src = e.target.result;
+          document.getElementById("image_preview").src = e.target.result;
       };
       reader.readAsDataURL(input.files[0]);
   }
   else {
-      document.getElementById("profile_image").src = "";
+      document.getElementById("image").src = "";
   }
 }
 
-
-
-//   const response_edit = await fetch('http://127.0.0.1:8000/users/profile/' + user_id + '/', {
-//     headers:{
-//         'Authorization': 'Bearer ' + localStorage.getItem("access"),
-//         'content-type':'application/json',
-//     },
-//     method:'PUT',
-//     body: JSON.stringify({
-//         "email": email,
-//         "password": password,
-//         "username": username,
-//         "bio": bio,
-//         "tags": tag,
-//     })
-// })
-// console.log(response_edit)
-// location.href = 'profile.html';
+function printVideoKey()  {
+  const video_key = document.getElementById('video_key').value;
+  document.getElementById("result").innerText = video_key;
+}
